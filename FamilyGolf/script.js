0c358a00c11d4265231af621c95b9138fa8a57e7
Vivek Jadhav <vivekjadhao555@gmail.com>
2023-12-18 13:29:35 +0530

    family golf

---
+++ FamilyGolf/script.js
@@ -0,0 +1,14 @@
+// GENERATE RANDOM COLOR
+
+// let randomColor=function(){
+//     let hex ="0123456789ABCDEF"
+//     let color = '# '
+// }
+
+let randomm='Vivek'
+
+for(const i of randomm){
+    console.log(i);
+}
+
+console.log('fffffffff');
