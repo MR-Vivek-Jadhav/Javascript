226e8394b7c76938c45fd3b347e38a0ed072511e
Vivek Jadhav <vivekjadhao555@gmail.com>
2024-01-28 19:45:29 +0530

    BgColor

--- Unlimited Color/script.js
+++ Unlimited Color/script.js
@@ -1,57 +1,61 @@
 // GENERATE RANDOM COLOR
 
-let randomColor=function(){
-    let hex ="0123456789ABCDEF"
-    let color = '#'
+let randomColor = function () {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
 
-   for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor( Math.random() * 16)]
-   }
-//    return color
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  //    return color
 
-// for(let i of hex){
-//    if(i<=6){
-//     color +=hex[Math.floor( Math.random() * 16)]
-// }
+  // for(let i of hex){
+  //    if(i<=6){
+  //     color +=hex[Math.floor( Math.random() * 16)]
+  // }
 
-    
-//    }
+  //    }
 
+  return color;
+};
 
-return color
-}
+let interValID;
+let startChangingColor = function () {
+  interValID = setInterval(function () {
+    document.body.style.backgroundColor = randomColor();
+  }, 1000);
+};
 
-let interValID
-let startChangingColor=function(){
-    interValID=setInterval(function(){
-     document.body.style.backgroundColor=randomColor()
-        },1000)
-}
+let stopChangingcolor = function () {
+  clearInterval(interValID);
+};
 
-let stopChangingcolor=function(){
-    clearInterval(interValID)
+document.querySelector("#start").addEventListener("click", startChangingColor);
 
-   }
+document.querySelector("#stop").addEventListener("click", stopChangingcolor);
 
+// *****************OR***********************
 
-document.querySelector('#start').addEventListener('click',startChangingColor)
+// let randomColor = function () {
+//     let ColorCode = "123456789ABCDEF";
 
-document.querySelector('#stop').addEventListener('click',stopChangingcolor)
+//     let Hash = "#";
 
+//     for (let i = 0; i < 6; i++) {
+//       Hash += ColorCode[Math.floor(Math.random() * 16)];
+//     }
+//     return Hash;
+//   };
+//   console.log(randomColor());
 
+//   let IntervalId;
 
-let BoyzObject={
-    student1:'dungya',
-    student2:'dhayrya',
-    student3:"kabir",
-    student14:"nahuShet",
-     student5:'Grace'
+//   document.querySelector("#start").addEventListener("click", function () {
+//     IntervalId = setInterval(() => {
+//       document.body.style.backgroundColor = randomColor();
+//     }, 1000);
+//   });
 
-}
-for(let i in BoyzObject){
-console.log(BoyzObject[i]);
-}
-
-BoyzObject.map((item)=>{
-return item
-})
+//   document.querySelector("#stop").addEventListener("click", function () {
+//     clearInterval(IntervalId);
+//   });
